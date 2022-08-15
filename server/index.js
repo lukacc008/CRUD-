ef48a3ae0b828ecaf433523fb8479610e00760b8
unknown <lukacc008@gmail.com>
2022-08-15 16:25:18 +0200

    fixing some problems

--- server/index.js
+++ server/index.js
@@ -29,6 +29,17 @@ app.post("/insert", async (req, res) => {
   }
 });
 
+app.get("/read", async (req, res) => {
+  FoodModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err)
+    }
+
+   res.send(result);
+
+  })
+});
+
 app.listen(3001, () => {
   console.log("Server running on port 3001");
 });
